d13fe4707b82bd8e6a322a0736aabe492e123b98
Shahin Rahimi <sh.rahimi.dev@gmail.com>
2024-10-22 11:38:59 +0330

    change gating logic

--- gating.py
+++ gating.py
@@ -1,14 +1,15 @@
+import os
 import numpy as np
 import flowkit as fk
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import seaborn as sns
-from scipy import stats
+import pandas as pd
+from scipy.stats import gaussian_kde
+from sklearn.linear_model import LinearRegression
+from sklearn.neighbors import KernelDensity
+from sklearn.model_selection import GridSearchCV
 from config import Config
-from utils import log
-import os
-# from sklearn.preprocessing import StandardScaler
-
 
 plt.switch_backend('agg')
 plt.ioff()
@@ -36,67 +37,73 @@ def check_channels(sample: fk.Sample) -> bool:
     return True
     
 
-def first_gating_plot(df, output_folder: str):
+def first_gating_plot(df: pd.DataFrame, output_folder: str) -> pd.DataFrame:
     x_label = Chan.FSC_A
     y_label = Chan.SSC_A 
     x = df[x_label]
     y = df[y_label]
-    plt.figure(figsize=(10,10))
-    plt.scatter(x, y,alpha=0.2, c='black', s=1)
-    sns.kdeplot(data=df, x = x_label, y = y_label,fill=False, alpha=1, levels=25, cmap="coolwarm",linewidths=1)
-    # Set axis limits
-    plt.xlim(0, 4.2e6)
-    plt.ylim(0, 4.2e6)
+    kde = gaussian_kde(np.vstack([x, y]), bw_method='scott')
+    # Create a grid to evaluate the KDE
+    x_min, x_max = x.min(), x.max()
+    y_min, y_max = y.min(), y.max()
+    X, Y = np.meshgrid(np.linspace(x_min, x_max, 100),
+                       np.linspace(y_min, y_max, 100))
+    positions = np.vstack([X.ravel(), Y.ravel()])
+    Z = kde(positions).reshape(X.shape)
+    
+    debris_threshold = 25000
+    max_fsc_a, max_ssc_a = np.max(x), np.max(y)
+    valid_points = (x> debris_threshold) & (y> debris_threshold) & (x < (max_fsc_a-1000)) & (y < (max_ssc_a-1000))
+    
+    plt.figure(figsize=(8,6))
+    plt.scatter(x, y, alpha=0.4, s=1, c='lightgrey', label='Original Data')
+    plt.scatter(x[valid_points], y[valid_points], alpha=0.4,  s=1, c='black', label='Gated Data (Gate 1)')
+    plt.contour(X, Y, Z, cmap='coolwarm', levels=25, linewidths=0.5)
     plt.title(f'{x_label} vs {y_label} Scatter Plot')
     plt.xlabel(x_label)
     plt.ylabel(y_label)
-    
+    plt.legend()
     plt.savefig(os.path.join(output_folder, "first_gating.pdf"), format="pdf")
-
-def second_gating_plot(df, output_folder: str, sd_df=2) -> plt:
     
-    def apply_filters(df):
-        # Filter out data points based on the specified criteria
-        df_filtered = df[(df[Chan.FSC_A] <= 4.2e6) & (df[Chan.FSC_A] >= 50000) &
-                        (df[Chan.SSC_A] <= 4.2e6) & (df[Chan.SSC_A] >= 50000)]
-        return df_filtered
+    filtered_df = df[valid_points]
+    return filtered_df
     
-    df = apply_filters(df)
 
+def second_gating_plot(df: pd.DataFrame, output_folder: str, sd_df=2) -> pd.DataFrame:
     x_label = Chan.FSC_A
-    y_label = Chan.FSC_H
-    x = df[x_label]
-    y = df[y_label]
-    # Perform linear regression to get the line of best fit
-    slope, intercept, r_value, p_value, std_err = stats.linregress(x, y)
-    # Calculate the fitted values (the line of best fit)
-    fitted_values = slope * x + intercept
-    # Calculate the residuals and standard deviation of residuals
-    residuals = y - fitted_values
-    std_dev = np.std(residuals)
-    # Define the upper and lower gate lines (default 2 standard deviations away)
-    upper_gate = fitted_values + (sd_df * std_dev)
-    lower_gate = fitted_values - (sd_df * std_dev)
-    within_gate = (y >= lower_gate) & (y <= upper_gate)
-    outside_gate = (y < lower_gate) | (y > upper_gate)
+    y_label = Chan.SSC_A
+    fsc_a = df[Chan.FSC_A].values
+    fsc_h = df[Chan.FSC_H].values
+    lr = LinearRegression()
+    
+    # Reshape data for linear regression
+    fsc_a_reshaped = fsc_a.reshape(-1, 1)
+    fsc_h_reshaped = fsc_h.reshape(-1, 1)
+    
+    # Fit linear regression model
+    lr.fit(fsc_a_reshaped, fsc_h_reshaped)
+    # Predict FSC-H values and calculate norms (distances from the fitted line)
+    predicted_fsc_h = lr.predict(fsc_a_reshaped).flatten()
+    norms = np.abs(fsc_h - predicted_fsc_h)
+    
+    # Calculate standard deviation of norms and apply the second gate (4 std deviations)
+    std_norm = np.std(norms)
+    gate_threshold = sd_df * std_norm
+
+    # Apply the second gate by excluding points outside the threshold
+    valid_points = norms < gate_threshold
+    filtered_data = df[valid_points]
     
     # Plot the data and the gating lines
-    plt.figure(figsize=(10, 10))
-    
-    # Scatter plot
-    plt.scatter(x[within_gate], y[within_gate], alpha=0.5, c='yellow', s=1, label="Within Gate")
-    plt.scatter(x[outside_gate], y[outside_gate], alpha=0.5, c='black', s=1, label="Outside Gate")
-    
+    plt.figure(figsize=(8, 8))
+    plt.scatter(fsc_a,fsc_h, c='lightgrey', s=1, label="Original Data")
+    plt.scatter(fsc_a[valid_points], fsc_h[valid_points], c='black',s=1 , label='Gated Data (Gate 2)')
     # Plot the line of best fit
-    plt.plot(x, fitted_values, color='red', label='Line of Best Fit')
-    
-    # Plot the upper and lower gating lines
-    plt.plot(x, upper_gate, color='green', label=f'Upper Gate ({sd_df} SD)')
-    plt.plot(x, lower_gate, color='blue', label=f'Lower Gate ({sd_df} SD)')
-    
-    # Set axis limits if needed
-    plt.xlim(0, 4.2e6)
-    plt.ylim(0, 4.2e6)
+    plt.plot(fsc_a, predicted_fsc_h, color='green', label='Fitted Line', linewidth=2)
+    upper_bound = predicted_fsc_h + gate_threshold
+    lower_bound = predicted_fsc_h - gate_threshold
+    plt.plot(fsc_a, upper_bound, color='red', linestyle='--', label='Upper Bound (+4σ)', linewidth=1)
+    plt.plot(fsc_a, lower_bound, color='red', linestyle='--', label='Lower Bound (-4σ)', linewidth=1)
     
     # Labeling the ticks with "5 positive decades"
     plt.gca().xaxis.set_major_formatter(ticker.FuncFormatter(lambda val, pos: f'{int(val):e}'))
@@ -112,83 +119,140 @@ def second_gating_plot(df, output_folder: str, sd_df=2) -> plt:
     
     plt.savefig(os.path.join(output_folder, "second_gating.pdf"), format="pdf")
     
-    return plt
+    return filtered_data
 
-def third_gating_plot(sample: fk.Sample, output_folder: str) -> float | str | None:
+def third_gating_plot(df: pd.DataFrame, output_folder: str) -> float | str | None:
     x_label = Chan.Comp_mEmerald_A
     y_label = Chan.Comp_mCherry_A
-    
+
+    sam = fk.Sample(df, sample_id="Gated Test")
+    df2 = sam.as_dataframe(source='raw')
+    x = df2[Chan.Comp_mEmerald_A]
+    y = df2[Chan.Comp_mCherry_A]
+    # specify transform values
+    max_value = max(x.max(), y.max())
+    rng = max(x.max() - x.min(), y.max() - y.min())
+    width = rng * Config.width_multi
     biex_xform = fk.transforms.WSPBiexTransform(
         'biex',
-        max_value=Config.max_value,
+        max_value=max_value,
         positive=Config.pos,
-        width=Config.width,
+        width=-width,
         negative=Config.neg
     )
     
-    sample.apply_transform(biex_xform)
-    df = sample.as_dataframe(source="xform")
-    # Plot the data and the gating lines
-    plt.figure(figsize=(10, 10))
-    x = df[x_label]
-    y = df[y_label]
-    # Determine the midpoint for gating (optional: use specific thresholds if needed)
-    x_median = np.median(x)
-    y_median = np.median(y)
+    sam.apply_transform(biex_xform)
+    df2 = sam.as_dataframe(source='xform')
     
-    # Define the quadrants
-    quadrant_1 = df[(df[x_label] > x_median) & (df[y_label] > y_median)]
-    quadrant_2 = df[(df[x_label] <= x_median) & (df[y_label] > y_median)]
-    quadrant_3 = df[(df[x_label] <= x_median) & (df[y_label] <= y_median)]
-    quadrant_4 = df[(df[x_label] > x_median) & (df[y_label] <= y_median)]
+    # new transformed value
+    x = df2[x_label]
+    y = df2[y_label]
+    # old untransformed values
+    xx = df[x_label]
+    yy = df[y_label]
     
-    # Calculate populations
-    D = len(quadrant_2)  # Double transfected cells
-    G = len(quadrant_3)  # mEmerald cells (Green)
-    R = len(quadrant_1)  # mCherry cells (Red)
-    U = len(quadrant_4)  # Untransfected cells
+    # Define autofluorescence cutoff
+    autofluorescence_cutoff = 4200
     
-    total_cells = len(df)
-    if total_cells == 0:
-        fetch_score = None
-    else:
-        fetch_score = D / (D + G + R) if (D + G + R) != 0 else None
-    
-    # Cases where the score should be null/error
-    if U / total_cells > 0.9:
-        fetch_score = 'error/null'
-    # Cases where the score should be error/null
-    elif fetch_score is not None and fetch_score > 0.9:
-        fetch_score = 'null/error'
-        
-    # Calculate KDE
-    kde = stats.gaussian_kde([x, y])
-    # Evaluate the KDE on a grid of points
-    # xmin, xmax = min(x), max(x)
-    # ymin, ymax = min(y), max(y)
-    # log(f"xmin: {xmin} x:max {xmax}")
-    # log(f"ymin: {ymin} y:max {ymax}")
-    
-    # xx, yy = np.mgrid[xmin:xmax:1000j, ymin:ymax:1000j]
-    # positions = np.vstack([xx.ravel(), yy.ravel()])
-    # density = np.reshape(kde(positions).T, xx.shape)
-    # log(f"min density: {density.min()} max density {density.max()}")
-    
-    # Specify custom levels
-    # levels = np.geomspace(4.6722750452452243e-08, density.max(), num=10)
-    #
-    #levels = np.linspace(density.min(), density.max(), 10) 
-    
-    sns.kdeplot(x=x, y=y,bw_adjust=0.3, cmap="coolwarm", fill=False, levels=30, thresh=0.25,linewidths=0.1)
+    # Kernel Density Estimation
+    data = np.vstack([x, y]).T
+    # Grid search over badwandwidth in log-space
+    bandwidths = np.logspace(-2, 1, 50)
+    grid = GridSearchCV(KernelDensity(), {'bandwidth': bandwidths}, cv=5)
+    grid.fit(data)
+    best_bandwidth = grid.best_estimator_.bandwidth_
 
-    plt.scatter(x,y, alpha=0.5, c='black', s=1)
-    plt.axvline(x=x_median, color='black', linestyle='--', linewidth=1)
-    plt.axhline(y=y_median, color='black', linestyle='--', linewidth=1)
+    kde = KernelDensity(bandwidth=best_bandwidth).fit(data)
+
+    # Filter density scores based on autofluorescence cutoff
+    valid_data = (x < autofluorescence_cutoff) & (y < autofluorescence_cutoff)
+    kde_values = kde.score_samples(np.vstack([x[valid_data], y[valid_data]]).T)
     
-    plt.title(f'{x_label} vs {y_label} Quadrant Gating')
-    plt.xlabel(x_label)
-    plt.ylabel(y_label)
+    # Get the contour levels
+    number_of_levels = 30
+    levels = np.linspace(0, kde_values.max(), number_of_levels)
+    # normalize levels [0,1]
+    min_val = levels.min()
+    max_val = levels.max()
     
-    plt.savefig(os.path.join(output_folder, "third_gating.pdf"), format="pdf")
+    normalize_levels = (levels - min_val) / (max_val - min_val)
+    sorted_normalize_levels = np.sort(normalize_levels)
     
+    plt.figure (figsize=(10, 10))
+    ax = sns.kdeplot(
+        x=x, 
+        y=y,
+        levels=sorted_normalize_levels, 
+        bw_method="scott",
+        cmap="coolwarm", 
+        fill=True, 
+        thresh=0.001, 
+        linewidths=0.1,
+        legend=True
+    )
+    
+   
+    def get_first_best_lines(paths, lvl):
+        for j,path in enumerate(paths):
+            level = sorted_normalize_levels[j % len(sorted_normalize_levels)]
+            if level < lvl:
+                continue
+            vertices = path.vertices
+            x_mean = np.mean(x)
+            y_mean = np.mean(y)
+            x_vertices = vertices[:, 0]
+            y_vertices = vertices[:, 1]
+            mask = (x_vertices < x_mean) & (y_vertices < y_mean) # grab Q4 points
+            valid_x_vertices = x_vertices[mask]
+            valid_y_vertices = y_vertices[mask]
+            vline = np.max(valid_x_vertices)
+            hline = np.max(valid_y_vertices)
+            return vline, hline
+        
+    target_contour = ax.collections[0] # collection has one contour level
+    paths = target_contour.get_paths()
+    vline, hline = get_first_best_lines(paths, 0.6)
+    
+    plt.figure (figsize=(10, 10))
+    sns.kdeplot(
+        x=x, 
+        y=y,
+        levels=sorted_normalize_levels, 
+        bw_method="scott",
+        cmap="coolwarm", 
+        fill=False, 
+        thresh=0.001, 
+        linewidths=1,
+        legend=True
+    )
+    quadrant_1 = df2[(df2[x_label] < vline) & (df2[y_label] > hline)] # HIGH Y and LOW X (High mCherry)
+    quadrant_3 = df2[(df2[x_label] > vline) & (df2[y_label] < hline)] # LOW Y and HIGH X (High eEmerald)
+    quadrant_2 = df2[(df2[x_label] > vline) & (df2[y_label] > hline)] # HIGH Y and HIGH X (Double Transfected)
+    quadrant_4 = df2[(df2[x_label] < vline) & (df2[y_label] > hline)] # LOW Y and LOW X (Double Transfected)
+    R = len(quadrant_1)  # mCherry cells (Red)
+    G = len(quadrant_3)  # mEmerald cells (Green)
+    D = len(quadrant_2)  # Double transfected cells
+    U = len(quadrant_4)  # Untransfected cells
+    fetch_score = D / (D + G + R) if (D + G + R) != 0 else None
+    if fetch_score is None:
+        fetch_score_string = "None"
+    else:
+        fetch_score_string = f'{fetch_score:.2f}'
+    
+    xlim = plt.gca().get_xlim()
+    ylim = plt.gca().get_ylim()
+    bbox=dict(facecolor='white', edgecolor='black', boxstyle='round,pad=0.5')
+    
+    plt.text(xlim[1], ylim[1], f'Q2 D: {D}', fontsize=10, verticalalignment='top', horizontalalignment='right', color='blue', bbox=bbox)
+    plt.text(xlim[0], ylim[1], f'Q1 R: {R}', fontsize=10, verticalalignment='top',horizontalalignment='left', color='red', bbox=bbox)
+    plt.text(xlim[1], ylim[0], f'Q3 G: {G}', fontsize=10, verticalalignment='bottom',horizontalalignment='right', color='green', bbox=bbox)
+    plt.text(xlim[0], ylim[0], f'Q4 U: {U}', fontsize=10, verticalalignment='bottom',horizontalalignment='left', color='black', bbox=bbox)
+    plt.scatter(x, y, alpha=0.1, c='black', s=1, label='Data Points')
+    plt.title(f'{x_label} vs {y_label} Scatter Plot\nfetch score: {fetch_score_string}')
+            
+    plt.axhline(y=hline, color='black', linestyle='--', linewidth=1)
+    plt.axvline(x=vline, color='black', linestyle='--', linewidth=1)
+    
+    plt.savefig(os.path.join(output_folder, 'third_gate.png'))
     return fetch_score
+    
